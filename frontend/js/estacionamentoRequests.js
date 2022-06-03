a9c3b9fe838cff82b78225a75616cc34d0f04b92
Sutanrrier <pabloryanbr0@gmail.com>
2022-06-03 15:34:26 -0300

    Consulta de Estacionamento's page created

--- frontend/js/estacionamentoRequests.js
+++ frontend/js/estacionamentoRequests.js
@@ -25,3 +25,41 @@ function salvarEstacionamento() {
 
     alert("Estacionamento cadastrado com sucesso!");
 }
+
+//GET - Retorna todos os carros dentro do banco e gera uma tabela com o JSON de resposta
+function criarTabelaEstacionamento() {
+    let tabela = document.getElementById("tabelaEstacionamento")
+
+    function criaLinha(objeto) {
+        let linha = document.createElement("tr");
+
+        // Criando as colunas da linha
+        let colunaId = document.createElement("td");
+        let colunaNome = document.createElement("td");
+        
+        //Injetando informação nas colunas da linha
+        colunaId.innerHTML = objeto.id;
+        colunaNome.innerHTML = objeto.nome;
+
+        //Colocando as colunas dentro da linha
+        linha.appendChild(colunaId);
+        linha.appendChild(colunaNome);
+
+        return linha;
+    }
+
+    const options = {
+        method: "GET",
+        mode: "cors",
+        cache: "default"
+    }
+
+    const url = "http://localhost:8080/estacionamentos"
+
+    fetch(url, options)
+        .then(response => response.json()
+            .then(data => (data.forEach(element => {
+                let linha = criaLinha(element);
+                tabela.appendChild(linha);
+            }))));
+}
