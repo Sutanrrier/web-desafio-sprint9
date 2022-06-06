8d0bea6f636265b7f46fc466310da02d19344950
Sutanrrier <pabloryanbr0@gmail.com>
2022-06-06 14:50:49 -0300

    Atualizar Estacionamento's page created

--- frontend/js/estacionamentoRequests.js
+++ frontend/js/estacionamentoRequests.js
@@ -2,28 +2,6 @@
 const myHeaders = new Headers();
 myHeaders.append("Content-type", "application/json; charset=UTF-8");
 
-//POST - Gera um JSON com os campos do formulario e envia como body da requisição
-function salvarEstacionamento() {
-    const nome = document.getElementById("nome").value;
-
-    const url = "http://localhost:8080/estacionamentos"
-    const body = {
-        "nome": nome
-    }
-    const options = {
-        method: "POST",
-        body: JSON.stringify(body),
-        mode: "cors",
-        cache: "default",
-        headers: myHeaders
-    }
-
-    fetch(url, options)
-        .then(response => {
-            (response.status == "201") ? alert("Estacionamento cadastrado com sucesso!") : alert("Ocorreu um erro!");
-        })
-}
-
 //Cria o cabeçalho da tabela
 function criaCabecalhoTabela() {
     const linha = document.createElement("tr");
@@ -118,6 +96,52 @@ function criarTabelaEstacionamentoId() {
             tabela.removeChild(childrensTabela[1]);
         });
 
+}
+
+//POST - Gera um JSON e envia como body da requisição para cadastrar um estacionamento
+function salvarEstacionamento() {
+    const nome = document.getElementById("nome").value;
+
+    const url = "http://localhost:8080/estacionamentos"
+    const body = {
+        "nome": nome
+    }
+    const options = {
+        method: "POST",
+        body: JSON.stringify(body),
+        mode: "cors",
+        cache: "default",
+        headers: myHeaders
+    }
+
+    fetch(url, options)
+        .then(response => {
+            (response.status == "201") ? alert("Estacionamento cadastrado com sucesso!") : alert("Ocorreu um erro!");
+        })
+}
+
+//PUT - Gera um JSON e envia como body da requisição para atualizar um estacionamento por seu Id
+function atualizarEstacionamento() {
+    const id = document.getElementById("id").value;
+    const nome = document.getElementById("nome").value;
+
+    const url = `http://localhost:8080/estacionamentos/${id}`;
+    const body = {
+        "id": id,
+        "nome": nome
+    }
+    const options = {
+        method: "PUT",
+        body: JSON.stringify(body),
+        mode: "cors",
+        cache: "default",
+        headers: myHeaders
+    }
+
+    fetch(url, options)
+        .then(response => {
+            (response.status == "201") ? alert("Estacionamento atualizado com sucesso!") : alert("Erro! -> Este ID não existe no banco!");
+        })
 }
 
 //DELETE - Apaga um carro dentro do banco apartir de um ID
